49fe8b3d0afae03750555ea841a70506041f4a4e
Billy Sjöberg <billy.sjoberg@svt.se>
2016-01-22 18:11:47 +0100

    Changed shutter time for camera

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var imgur = require('imgur-node-api');
 var path = require('path');
 var secrets = require('./secrets');
 var SlackClient = require('slack-client');
-var sys = require('sys');
 var exec = require('child_process').exec;
 var fs = require('fs');
 
@@ -10,10 +9,9 @@ var conf = {
     slackToken : secrets.slackToken,
     imgurKey : secrets.imgurKey,
     newCoffeeTrigger : 'Nu finns det nybryggt kaffe!',
-    thankYouText: 'Ge en applåd till den som fyllde på! ',
     localImage : '/tmp/ourhero.jpg',
-    //photoCommand : 'imagesnap -w 2.0 ' //should be replaced if on a raspberry with the equivalent command
-    photoCommand : 'fswebcam 640x480 ' //should be replaced if on a raspberry with the equivalent command
+    //photoCommand : 'imagesnap -w 2.0' //mac os
+    photoCommand : 'fswebcam -r 640x480 -S 20' //debian (r=resolution S=frames to skip, more light)
 };
 
 imgur.setClientID(conf.imgurKey);
@@ -34,30 +32,36 @@ slackClient.on('message', function(message) {
     if (message.user == bot.id) return; // Ignore bot's own messages
     if (!message.text || message.text.indexOf(conf.newCoffeeTrigger) == -1) return;
 
+    //remove previous photo if exists
     try{
         fs.unlinkSync(conf.localImage);
     }catch (ex){
         console.log("failed to delete old photo. " + ex);
     }
 
-    exec(conf.photoCommand + conf.localImage, function (error, stdout, stderr) {
-        sys.print('stdout: ' + stdout);
-        sys.print('stderr: ' + stderr);
+    //execute os command to take photo
+    exec(conf.photoCommand + ' ' + conf.localImage, function (error, stdout, stderr) {
+        var channel = slackClient.getChannelGroupOrDMByID(message.channel);
+
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
         if (error !== null) {
             console.log('exec error: ' + error);
+            channel.send('Misslyckades att ta bild på vår hjälte: ' + error);
             return;
         }
 
-        var channel = slackClient.getChannelGroupOrDMByID(message.channel);
-
+        //upload it to imgur
         imgur.upload(conf.localImage, function (err, res) {
             if(err){
                 console.log(err);
+                channel.send('Kunde inte ladda upp bild på vår anonyme hjälte: ', err);
                 return;
             }
 
+            //link the imgur photo in a slack message
             console.log('Uploaded image to:' + res.data.link); // Log the imgur url
-            channel.send(conf.thankYouText + res.data.link);
+            channel.send('Ge en applåd till den som fyllde på! ' + res.data.link);
         });
     });
 });
